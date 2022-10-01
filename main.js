d2a87b83effece805303c19fed682d01855917d5
LoonyYesh <pyeswanth131@gmail.com>
2022-10-01 10:13:27 +0530

    comiit 3

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ function solveIt(num){
         sum = sum + num % 10;
         num = ~~(num / 10);
     }
-    document.getElementById("ans").innerHTML = sum;
+    return sum;
+}
+function processIt(){
+    var input = document.getElementById("input").value;
+    var total = document.getElementById("total").value;
+    input = input.replace(" ","");
+    const myarr = input.split(",");
+    myarr.forEach(num => {
+        let temp = solveIt(num);
+        if(temp==total){
+            document.getElementById("ans").innerHTML += "," + num;
+        }
+    });
 }

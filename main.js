5f312bff372cfbe3becc8caca9f7e23cdd566b42
LoonyYesh <pyeswanth131@gmail.com>
2022-10-01 09:45:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function solveIt(num){
     var sum = 0;
     while (num>0) {
         sum = sum + num % 10;
-        num = num / 10;
+        num = ~~(num / 10);
     }
     document.getElementById("ans").innerHTML = sum;
 }
